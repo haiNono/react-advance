275c7ec433fa1f6638075c35ff87c935ac18ca73
hainuo <hainuo.li@transwarp.io>
2023-07-30 15:24:01 +0800

    feat: 弹出配置&&解决报错

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,15 @@ module.exports = {
   extends: ['react-app', 'react-app/jest'],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
+    // 解决 Using `babel-preset-react-app` requires that you specify `NODE_ENV` or `BABEL_ENV` environment variables.
+    // 这个不影响运行的报错的配置
+    babelOptions: {
+      presets: [
+        ['babel-preset-react-app', false],
+        'babel-preset-react-app/prod'
+      ]
+    }
   },
   plugins: ['react'],
   rules: {
--- src/components/ref/Son/index.jsx
+++ src/components/ref/Son/index.jsx
@@ -4,3 +4,5 @@ export default function Son(props) {
   const sonRef = props.sonRef
   return <div ref={sonRef}>Son</div>
 }
+process.env.TEST = 'test'
+console.log(process.env, 'son')
