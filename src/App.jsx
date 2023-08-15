da4d38d8e6c06bd101a9e09d9ad769b3d1fc6b35
hainuo <hainuo.li@transwarp.io>
2023-08-15 11:50:21 +0800

    feat: hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import Pure from './components/RenderOptimize/pure'
+import React, { useState } from 'react'
 
 function App() {
-  return <Pure />
+  let num, setnum
+  const [number, setnumber] = useState(0)
+  ;[num, setnum] = useState(1)
+  const [err, seterr] = useState('error')
+  return (
+    <>
+      <h2>
+        {number}
+        <button onClick={() => setnumber(number + 1)}>点击</button>
+      </h2>
+      <h2>
+        {num}
+        <button onClick={() => setnum(num + 1)}>点击</button>
+      </h2>
+      <h2>
+        {err}
+        <button onClick={() => seterr('good')}>点击</button>
+      </h2>
+    </>
+  )
 }
 
 export default App
